4808ce38b983a1d459d0a0954e83d399065dd445
Lokii808 <64386219+Lokii808@users.noreply.github.com>
2024-03-27 14:02:22 -0300

    Update aa.php

--- aa.php
+++ aa.php
@@ -1,6 +1,6 @@
 <?php
 // Redireciona para o site desejado
-$novoSite = "http://www.evil.com";
+$novoSite = "https://webhook.site/479c52af-1ca1-425f-9553-bb7de85211e3";
 header("Location: $novoSite");
 exit; // Certifique-se de encerrar o script após o redirecionamento
 ?>
