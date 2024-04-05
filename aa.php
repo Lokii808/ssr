2028c6c9de3676712c857a94db9f928e8f0b616b
Xandsz <64386219+Lokii808@users.noreply.github.com>
2024-04-05 10:24:49 -0300

    Update aa.php

--- aa.php
+++ aa.php
@@ -1,6 +1,6 @@
 <?php
 // Redireciona para o site desejado
-$novoSite = "https://webhook.site/479c52af-1ca1-425f-9553-bb7de85211e3";
+$novoSite = "gopher://i4nwqx8kvrk2huqnlg7e3lamedk48uwj.oastify.com";
 header("Location: $novoSite");
 exit; // Certifique-se de encerrar o script após o redirecionamento
 ?>
