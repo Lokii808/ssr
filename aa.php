5bcca1864164d316f7879fe782b91b0e2cad10e0
Xandsz <64386219+Lokii808@users.noreply.github.com>
2024-04-05 10:25:25 -0300

    Update aa.php

--- aa.php
+++ aa.php
@@ -1,6 +1,6 @@
 <?php
 // Redireciona para o site desejado
-$novoSite = "gopher://i4nwqx8kvrk2huqnlg7e3lamedk48uwj.oastify.com";
+$novoSite = "https://i4nwqx8kvrk2huqnlg7e3lamedk48uwj.oastify.com";
 header("Location: $novoSite");
 exit; // Certifique-se de encerrar o script após o redirecionamento
 ?>
